246a7f7baf67c2a593a2a99237a17c7e723181d3
Nathanael Chandra Aditya <nthnlchndr@student.ub.ac.id>
2022-12-04 21:59:01 +0700

    Chandra2

---
+++ app/src/main/java/com/example/myapplication/ItemDescActivity.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class ItemDescActivity extends AppCompatActivity {
+    private Button AddtoCartbtn;
+    private String nama, harga, desc, jumlahItem;
+    private ImageView tambah, kurang, gambar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_desc);
+        initView();
+        getBundle();
+    }
+
+    private void getBundle() {
+    }
+
+    private void initView(){
+        AddtoCartbtn = findViewById(R.id.AddToCartButton);
+        nama = findViewById(R.id.NamaItem);
+        harga = findViewById(R.id.HargaItem);
+        desc = findViewById(R.id.DeskripsiItem);
+        jumlahItem = findViewById(R.id.JumlahItem);
+        tambah = findViewById(R.id.TambahJumlahItem);
+        kurang = findViewById(R.id.KurangJumlahItem);
+        gambar = findViewById(R.id.GambarItem);
+    }
+
+}
