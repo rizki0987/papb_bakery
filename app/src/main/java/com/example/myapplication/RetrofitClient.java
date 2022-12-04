24055e9af7fdee83618b43ab7e434e3f24b23e7b
irvanhanif <irhan8803@gmail.com>
2022-12-05 02:18:16 +0700

    add header reader

--- app/src/main/java/com/example/myapplication/RetrofitClient.java
+++ app/src/main/java/com/example/myapplication/RetrofitClient.java
@@ -1,6 +1,11 @@
 package com.example.myapplication;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -8,9 +13,18 @@ public class RetrofitClient {
     public static final String BASE_URL = "http://192.168.1.71:4500/api/";
     private static Retrofit retrofit = null;
 
-    public static Retrofit getClient() {
+    public static Retrofit getClient(String token) {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
+                    .client(new OkHttpClient.Builder().addInterceptor(new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request newRequest = chain.request().newBuilder()
+                                    .addHeader("Authorization", "Bearer " + token)
+                                    .build();
+                            return chain.proceed(newRequest);
+                        }
+                    }).build())
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
--- app/src/main/java/com/example/myapplication/menuService.java
+++ app/src/main/java/com/example/myapplication/menuService.java
@@ -4,15 +4,19 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface menuService {
     @GET("menu")
     Call<List<menu>> listMenu();
 
-    @Headers({"Content-Type: application/json;charset=UTF-8"})
     @POST("menu")
     Call<apiResponse> addMenu(@Body menu Menu);
+
+    @DELETE("menu/{id}")
+    Call<apiResponse> deletMenu(@Path("id") int id);
 }
