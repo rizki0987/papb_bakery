28018c53b2832184c0013209c6d25698a2fed438
irvanhanif <irhan8803@gmail.com>
2022-12-05 22:57:37 +0700

    update login activity

--- app/src/main/java/com/example/myapplication/loginActivity.java
+++ app/src/main/java/com/example/myapplication/loginActivity.java
@@ -2,15 +2,28 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class loginActivity extends AppCompatActivity {
+public class loginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+        back = findViewById(R.id.imageButton);
+        back.setOnClickListener(this);
     }
 
 
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.imageButton){
+            this.finish();
+        }
+    }
 }
