8a0859f5f0b3e2c05dae2e8ac993f603390d3a1a
rizki0987 <113367468+rizki0987@users.noreply.github.com>
2022-12-05 22:34:10 +0700

    class Jva

---
+++ app/src/main/java/com/example/myapplication/registActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_page);
+    }
+
+
+}
