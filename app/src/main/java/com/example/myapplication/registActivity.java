ab44aa317708ca43291183966c4a12f0f9269576
rizki0987 <113367468+rizki0987@users.noreply.github.com>
2022-12-05 23:02:53 +0700

    connect regist

--- app/src/main/java/com/example/myapplication/registActivity.java
+++ app/src/main/java/com/example/myapplication/registActivity.java
@@ -3,8 +3,14 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class registActivity extends AppCompatActivity {
+    private Button regist, loginHere;
+    private TextView nama, email, pws, confirmpws;
+    private ImageView backToRegist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +18,14 @@ public class registActivity extends AppCompatActivity {
         setContentView(R.layout.register_page);
     }
 
+    private void initView(){
+        backToRegist = findViewById(R.id.imageButton);
+        regist = findViewById(R.id.button2);
+        loginHere = findViewById(R.id.button);
+        nama = findViewById(R.id.editTextTextEmailAddress2);
+        email = findViewById(R.id.editTextTextEmailAddress);
+        pws = findViewById(R.id.editTextTextEmailAddress3);
+        confirmpws = findViewById(R.id.editTextTextPassword);
+    }
 
 }
