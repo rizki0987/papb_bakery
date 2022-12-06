3395cc4799edb8a4e5f69181ab02fb5b56c8d6a5
rizki0987 <113367468+rizki0987@users.noreply.github.com>
2022-12-06 17:37:11 +0700

    addAct.java

--- app/src/main/java/com/example/myapplication/addActivity.java
+++ app/src/main/java/com/example/myapplication/addActivity.java
@@ -1,16 +1,16 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 
-public class update_desc extends AppCompatActivity implements View.OnClickListener {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class addActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.update_desc);
+        setContentView(R.layout.add_menu);
     }
 
 
