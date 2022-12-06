f4a256e624d67b700ceb04f7981d9244580d45b0
irvanhanif <irhan8803@gmail.com>
2022-12-06 15:07:40 +0700

    add adapter for menu

--- app/src/main/java/com/example/myapplication/homeActivity.java
+++ app/src/main/java/com/example/myapplication/homeActivity.java
@@ -1,15 +1,29 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class homeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    RecyclerView rvMenu;
+    menuAdapter adapter;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +31,35 @@ public class homeActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.homepage);
         ImageView btn = findViewById(R.id.imageView16);
         btn.setOnClickListener(this);
+        menuService menuService = RetrofitClient.getClient("").create(menuService.class);
+        Call<List<menu>> request = menuService.listMenu();
+        request.enqueue(new Callback<List<menu>>() {
+            @Override
+            public void onResponse(Call<List<menu>> call, Response<List<menu>> response) {
+                if(response.isSuccessful()){
+                    List<menu> list = response.body();
+//                    Log.d("success", "list " + list.getData());
+                    adapter = new menuAdapter(homeActivity.this, list, new menuAdapter.ItemClickListener() {
+                        @Override
+                        public void onItemClick(menu Menu) {
+                            intent = new Intent(homeActivity.this, ItemDescActivity.class);
+                            intent.putExtra("id_menu", Menu.getId_menu());
+                            startActivity(intent);
+                        }
+                    });
+                }else {
+                    String error = apiResponse.readError(response.errorBody());
+                    Log.d("errt", "" + error);
+                    Toast.makeText(homeActivity.this, error, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<menu>> call, Throwable t) {
+                Log.d("DataModel", "" + t.getMessage());
+                Toast.makeText(getApplicationContext(), "Error " + t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
---
+++ app/src/main/java/com/example/myapplication/menuAdapter.java
@@ -0,0 +1,64 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class menuAdapter extends RecyclerView.Adapter<menuAdapter.menuViewHolder> {
+    LayoutInflater inflater;
+    Context _context;
+    List<menu> menuList;
+    private ItemClickListener mItemListener;
+
+    public menuAdapter(Context _context, List<menu> menuList, ItemClickListener mItemListener) {
+        this._context = _context;
+        this.menuList = menuList;
+        this.inflater = LayoutInflater.from(this._context);
+        this.mItemListener = mItemListener;
+    }
+
+    @NonNull
+    @Override
+    public menuViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.row_menu, parent, false);
+        return new menuViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull menuViewHolder holder, int position) {
+        menu Menu = new menu();
+        holder.nama.setText(Menu.getNama());
+        holder.stok.setText(Menu.getStok());
+        holder.harga.setText(Menu.getHarga());
+    }
+
+    @Override
+    public int getItemCount() {
+        return menuList.size();
+    }
+
+    public interface ItemClickListener{
+        void onItemClick(menu Menu);
+    }
+
+    class menuViewHolder extends RecyclerView.ViewHolder {
+
+        TextView nama;
+        TextView harga;
+        TextView stok;
+
+        public menuViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nama = itemView.findViewById(R.id.tvNama);
+            harga = itemView.findViewById(R.id.tvHarga);
+            stok = itemView.findViewById(R.id.tvStok);
+        }
+    }
+}
